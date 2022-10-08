b02ca2057e94d350d1174b467bfeed7bd68eda6f
wise-guru <wise.guru123@gmail.com>
2022-10-08 15:43:00 -0400

    Add skill svgs

---
+++ src/components/Skills.js
@@ -0,0 +1,35 @@
+import { ReactComponent as HTML } from "../assets/skills/html.svg";
+import { ReactComponent as CSS } from "../assets/skills/css.svg";
+import { ReactComponent as React } from "../assets/skills/react.svg";
+import { ReactComponent as Nodejs } from "../assets/skills/nodejs.svg";
+import { ReactComponent as Git } from "../assets/skills/git.svg";
+import { ReactComponent as GithubLight } from "../assets/skills/github-light.svg";
+import { ReactComponent as Eslint } from "../assets/skills/eslint.svg";
+import { ReactComponent as Npm } from "../assets/skills/npm.svg";
+import { ReactComponent as Prettier } from "../assets/skills/prettier.svg";
+import { ReactComponent as Webpack } from "../assets/skills/webpack.svg";
+import { ReactComponent as JS } from "../assets/skills/javascript.svg";
+import { ReactComponent as Jest } from "../assets/skills/jest.svg";
+import { ReactComponent as Firebase } from "../assets/skills/firebase.svg";
+// import { ReactComponent as  } from "../assets/skills/.svg";
+
+function Skills() {
+  return (
+    <div className="skills-container">
+      <HTML />
+      <CSS />
+      <React />
+      <Git />
+      <GithubLight />
+      <Eslint />
+      <Npm />
+      <Prettier />
+      <Webpack />
+      <JS />
+      <Jest />
+      <Firebase />
+    </div>
+  );
+}
+
+export default Skills;
