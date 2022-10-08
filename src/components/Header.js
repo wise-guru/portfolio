900b5d4f3b066a8e5b450222ba410f2ec142024b
wise-guru <wise.guru123@gmail.com>
2022-10-08 14:03:32 -0400

    Initial layout

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="name">Wise Guru</div>
+      <ul className="links">
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </header>
+  );
+}
