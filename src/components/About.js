e0164b2f1e29c7e30bf945e1d9333de413db487d
wise-guru <wise.guru123@gmail.com>
2022-10-08 23:21:22 -0400

    Add about description

---
+++ src/components/About.js
@@ -0,0 +1,25 @@
+function About() {
+  return (
+    <div>
+      <p>Hi! I&apos;m Wise, a web dveloper.</p>
+
+      <p>
+        As a musician, I&apos;ve noticed many similarities between learning an instrument and
+        learning to program. Both teach you lessons in starting small and progressing incrementally.
+        There's no limit to what you can accomplish; people are constantly pushing the boundaries of
+        what we thoguth was possible both creatively and technically.
+      </p>
+
+      <p>
+        Learning web development has been challenging but extremely rewardng. With music, it's
+        exciting to be able to think of a melody in my head and then play it instantly. In web
+        development, I've been able to do the same. I can think of ideas for projects, and bring
+        them to life.
+      </p>
+
+      <p></p>
+    </div>
+  );
+}
+
+export default About;
