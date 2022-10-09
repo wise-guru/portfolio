ce0b3b25f4ebd7bd758e5557e02f3d7cec81e540
wise-guru <wise.guru123@gmail.com>
2022-10-08 23:20:31 -0400

    Add light mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,14 @@
-function Header() {
+import { ReactComponent as Sun } from "../assets/sun.svg";
+import { ReactComponent as Moon } from "../assets/moon.svg";
+import { useState } from "react";
+
+function Header(props) {
+  const { lightModeOn, setLightModeOn } = props;
+
+  const toggleMode = () => {
+    setLightModeOn((current) => !current);
+  };
+
   return (
     <header className="header">
       <div className="name">Wise Guru</div>
@@ -8,6 +18,25 @@ function Header() {
         <li>Projects</li>
         <li>Contact</li>
       </ul>
+      {lightModeOn ? (
+        <button
+          className="toggle-mode moon"
+          onClick={() => {
+            toggleMode();
+          }}>
+          <Moon />
+        </button>
+      ) : (
+        <button
+          className="toggle-mode sun"
+          onClick={() => {
+            toggleMode();
+          }}>
+          <Sun />
+        </button>
+      )}
     </header>
   );
 }
+
+export default Header;
