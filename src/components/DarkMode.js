3af579740e7aa89d3e18165ebd1b154ec9b94415
wise-guru <wise.guru123@gmail.com>
2022-10-12 23:12:40 -0400

    Add dark mode

---
+++ src/components/DarkMode.js
@@ -0,0 +1,51 @@
+import { ReactComponent as Sun } from "../assets/sun.svg";
+import { ReactComponent as Moon } from "../assets/moon.svg";
+
+function DarkMode(props) {
+  const { lightModeOn, setLightModeOn } = props;
+  let clickedClass = "clicked";
+  const body = document.body;
+  const lightTheme = "light";
+  const darkTheme = "dark";
+  let theme;
+
+  if (localStorage) {
+    theme = localStorage.getItem("theme");
+  }
+
+  if (theme === lightTheme || theme === darkTheme) {
+    body.classList.add(theme);
+  } else {
+    body.classList.add(lightTheme);
+  }
+
+  const switchTheme = (e) => {
+    if (theme === darkTheme) {
+      body.classList.replace(darkTheme, lightTheme);
+      e.target.classList.remove(clickedClass);
+      localStorage.setItem("theme", "light");
+      theme = lightTheme;
+      setLightModeOn(theme);
+    } else {
+      body.classList.replace(lightTheme, darkTheme);
+      e.target.classList.add(clickedClass);
+      localStorage.setItem("theme", "dark");
+      theme = darkTheme;
+      setLightModeOn(theme);
+    }
+  };
+
+  return (
+    <button
+      className={theme === "dark" ? clickedClass : ""}
+      id="darkMode"
+      onClick={(e) => {
+        switchTheme(e);
+        console.log(lightModeOn);
+      }}>
+      {lightModeOn === "light" ? <Moon /> : <Sun />}
+    </button>
+  );
+}
+
+export default DarkMode;
