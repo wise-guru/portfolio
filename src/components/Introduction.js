422569206154676dd4af992fab745f9a00431462
wise-guru <wise.guru123@gmail.com>
2022-10-13 00:28:04 -0400

    Add span

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -5,7 +5,9 @@ function Introduction(props) {
   return (
     <section>
       <div className="introduction">
-        <div>Greetings, I&apos;m Wise!</div>
+        <div className="title">
+          Hi, I&apos;m <span className="title-second">Myla.</span>
+        </div>
         <div className="transition" onClick={() => handleClick("about")}>
           <div>Get to know me</div>
           <Arrow />
