b0f07b273a951c958168dcea4e45a2976ca02bdb
wise-guru <wise.guru123@gmail.com>
2022-10-12 23:21:36 -0400

    Add scroll for sections

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 function App() {
+  //   const [lightModeOn, setLightModeOn] = useState(true);
+
+  const [lightModeOn, setLightModeOn] = useState(
+    localStorage ? localStorage.getItem("theme") : "light"
+  );
+
+  const aboutRef = useRef(null);
+  const projectsRef = useRef(null);
+  const contactRef = useRef(null);
+  const skillsRef = useRef(null);
+
+  const handleClick = (e) => {
+    if (e === "about") {
+      aboutRef.current?.scrollIntoView({ behavior: "smooth" });
+    } else if (e === "projects") {
+      projectsRef.current?.scrollIntoView({ behavior: "smooth" });
+    } else if (e === "contact") {
+      contactRef.current?.scrollIntoView({ behavior: "smooth" });
+    } else if (e === "skills") {
+      skillsRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header lightModeOn={lightModeOn} setLightModeOn={setLightModeOn} handleClick={handleClick} />
+      <About aboutRef={aboutRef} lightModeOn={lightModeOn} />
+      <Skills lightModeOn={lightModeOn} skillsRef={skillsRef} />
+      <Projects lightModeOn={lightModeOn} projectsRef={projectsRef} />
+      <Footer lightModeOn={lightModeOn} contactRef={contactRef} />
     </div>
   );
 }
