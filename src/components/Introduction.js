3c3ab0478ee11e662acec744d9c18e95c77f58e3
wise-guru <wise.guru123@gmail.com>
2022-10-12 23:11:01 -0400

    Remove commented code

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,8 +1,17 @@
-function Introduction() {
+import { ReactComponent as Arrow } from "../assets/arrow.svg";
+
+function Introduction(props) {
+  const { handleClick } = props;
   return (
-    <div>
-      <div>Greetings</div>
-    </div>
+    <section>
+      <div className="introduction">
+        <div>Greetings, I&apos;m Wise!</div>
+        <div className="transition" onClick={() => handleClick("about")}>
+          <div>Get to know me</div>
+          <Arrow />
+        </div>
+      </div>
+    </section>
   );
 }
 
